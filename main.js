70c88ee831f9afd4e5870fa4c7ab5ce065d185cf
Haider-Azawi <haider.azawi@outlook.com>
2017-03-15 09:57:00 -0700

    User can view items for sale

--- main.js
+++ main.js
@@ -3,35 +3,55 @@
 var items = [
   {
     itemName: 'Toy',
-    itemDescription: 'Lego toy for kids',
+    itemDescription: 'Lego toy for kids ',
     link: 'https://cdn.pixabay.com/photo/2016/07/19/23/38/superman-1529274__340.jpg'
   },
   {
     itemName: 'Bike',
-    itemDescription: '26" Mens Bike',
+    itemDescription: '26" Mens Bike ',
+    link: 'http://cdn2.ternbicycles.com/sites/default/files/images/bikes/unfolded/2016/11/linka7-g1-unfold-m0-dkblue.jpg'
+  },
+  {
+    itemName: 'Toy2',
+    itemDescription: 'Lego toy for kids ',
+    link: 'https://cdn.pixabay.com/photo/2016/07/19/23/38/superman-1529274__340.jpg'
+  },
+  {
+    itemName: 'Bike2',
+    itemDescription: '26" Mens Bike ',
     link: 'http://cdn2.ternbicycles.com/sites/default/files/images/bikes/unfolded/2016/11/linka7-g1-unfold-m0-dkblue.jpg'
   }
 ]
+var $container = document.createElement('div')
+var $row = document.createElement('div')
+document.body.appendChild($container)
+$container.appendChild($row)
+$container.classList.add('container-fluid')
+$row.classList.add('row')
 
-items.forEach(function(item) {
-  var $container = document.createElement('div')
-  var $row = document.createElement('div')
+items.forEach(function (item) {
+  renderItem(item)
+})
+
+function renderItem(item) {
   var $col = document.createElement('div')
   var $img = document.createElement('img')
   var $name = document.createElement('h4')
   var $description = document.createElement('p')
+  var $details = document.createElement('a')
+  $details.textContent = '<<  View Details'
+  $details.setAttribute('href', '#')
   $name.textContent = item.itemName
   $description.textContent = item.itemDescription
   $img.setAttribute('src', item.link)
-  document.body.appendChild($container)
-  $container.appendChild($row)
+  $img.setAttribute('width', 350)
+  $img.setAttribute('height', 200)
   $row.appendChild($col)
   $col.appendChild($img)
   $col.appendChild($name)
   $col.appendChild($description)
-  $container.classList.add('container')
-  $row.classList.add('row')
-  $col.classList.add('col-xs-6')
+  $description.appendChild($details)
+  $col.classList.add('col-xs-4')
   $col.classList.add('col')
   $img.classList.add('img')
-})
+}
